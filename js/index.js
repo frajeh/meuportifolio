e032b815f03a691bbe7e1e3191e1c83cb36e3a7d
frajeh <franciscojosemusic131@gmail.com>
2022-05-27 17:21:43 -0300

    Novos aquivos criados

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-alert("Olá, mundo")
+
+
+//sequencia de comandos para github
+
+/*
+
+1 - git status
+2 - git add .
+3 - git status
+4 - git commit -m "Index.html e js iniciais"
+5 - git checkout -b novo-potifolio
+6 - git push origin novo-potifolio
+
+meu token   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
+
+*/
